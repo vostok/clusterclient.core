bd096ba0f04460645a908e668784397fac96952f
epeshk <peshkov@skbkontur.ru>
2018-09-24 17:55:27 +0500

    use ClusterClientDetaing for LoggingOptions

--- Vostok.ClusterClient.Core/Misc/LoggingOptions.cs
+++ Vostok.ClusterClient.Core/Misc/LoggingOptions.cs
@@ -6,30 +6,30 @@ namespace Vostok.ClusterClient.Core.Misc
         /// <para>Gets or sets whether to log request details before execution.</para>
         /// <para>This parameter is optional and has a default value (see <see cref="ClusterClientDefaults.LogRequestDetails"/>).</para>
         /// </summary>
-        public bool LogRequestDetails { get; set; }
+        public bool LogRequestDetails { get; set; } = ClusterClientDefaults.LogRequestDetails;
 
         /// <summary>
         /// <para>Gets or sets whether to log result details after execution.</para>
         /// <para>This parameter is optional and has a default value (see <see cref="ClusterClientDefaults.LogResultDetails"/>).</para>
         /// </summary>
-        public bool LogResultDetails { get; set; }
+        public bool LogResultDetails { get; set; } = ClusterClientDefaults.LogResultDetails;
 
         /// <summary>
         /// <para>Gets or sets whether to log requests to each replica.</para>
         /// <para>This parameter is optional and has a default value (see <see cref="ClusterClientDefaults.LogReplicaRequests"/>).</para>
         /// </summary>
-        public bool LogReplicaRequests { get; set; }
+        public bool LogReplicaRequests { get; set; } = ClusterClientDefaults.LogReplicaRequests;
 
         /// <summary>
         /// <para>Gets or sets whether to log results from each replica.</para>
         /// <para>This parameter is optional and has a default value (see <see cref="ClusterClientDefaults.LogReplicaResults"/>).</para>
         /// </summary>
-        public bool LogReplicaResults { get; set; }
+        public bool LogReplicaResults { get; set; } = ClusterClientDefaults.LogReplicaResults;
 
         /// <summary>
-        /// <para>Gets or sets whether to add sequential log prefix to each request's messages.</para>
+        /// <para>Gets or sets whether to add sequential log prefix to each request message.</para>
         /// <para>This parameter is optional and has a default value (see <see cref="ClusterClientDefaults.LogPrefixEnabled"/>).</para>
         /// </summary>
-        public bool LogPrefixEnabled { get; set; }
+        public bool LogPrefixEnabled { get; set; } = ClusterClientDefaults.LogPrefixEnabled;
     }
 }
