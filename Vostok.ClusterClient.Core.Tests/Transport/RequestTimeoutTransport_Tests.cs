48eb18138764c73a15a67888b3d2389908053f42
epeshk <peshkov@skbkontur.ru>
2018-09-25 14:17:22 +0500

    add RequestTimeoutTransport_Tests

---
+++ Vostok.ClusterClient.Core.Tests/Transport/RequestTimeoutTransport_Tests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using FluentAssertions.Extensions;
+using NSubstitute;
+using NUnit.Framework;
+using Vostok.ClusterClient.Core.Model;
+using Vostok.ClusterClient.Core.Transport;
+
+namespace Vostok.ClusterClient.Core.Tests.Transport
+{
+    [TestFixture]
+    internal class RequestTimeoutTransport_Tests
+    {
+        private ITransport baseTransport;
+        private RequestTimeoutTransport timeoutTransport;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            baseTransport = Substitute.For<ITransport>();
+            timeoutTransport = new RequestTimeoutTransport(baseTransport);
+        }
+
+        [TestCase(0.111)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void Send_append_request_timeout_header(double seconds)
+        {
+            var timeout = seconds.Seconds();
+            var request = Request.Get("http://a/");
+
+            var passed = false;
+            
+            baseTransport.WhenForAnyArgs(x => x.SendAsync(default, default, default)).Do(
+                c => passed = c.Arg<Request>().Headers[HeaderNames.RequestTimeout] == timeout.Ticks.ToString());
+            
+            timeoutTransport.SendAsync(request, timeout, default).GetAwaiter().GetResult();
+
+            passed.Should().BeTrue();
+        }
+    }
+}
