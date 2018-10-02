24b9771f424cf45301e3826712173dfe5219eeb1
epeshk <peshkov@skbkontur.ru>
2018-10-02 13:50:04 +0500

    restore missing tests

---
+++ Vostok.ClusterClient.Core.Tests/Model/RequestQueryExtensions_Tests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Vostok.ClusterClient.Core.Model;
+
+namespace Vostok.ClusterClient.Core.Tests.Model
+{
+    [TestFixture]
+    internal class RequestQueryExtensions_Tests
+    {
+        private Request request;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            request = Request.Get("foo/bar?a=1");
+        }
+
+        [Test]
+        public void WithAdditionalQueryParameter_should_correctly_add_parameter_with_string_value()
+        {
+            request.WithAdditionalQueryParameter("b", "2").Url.ToString().Should().Be("foo/bar?a=1&b=2");
+        }
+
+        [Test]
+        public void WithAdditionalQueryParameter_should_correctly_add_parameter_with_typed_object_value()
+        {
+            request.WithAdditionalQueryParameter("b", 2).Url.ToString().Should().Be("foo/bar?a=1&b=2");
+        }
+    }
+}
