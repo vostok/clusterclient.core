623e888469ddd8cc56c00707a1eb8cf30c67624a
epeshk <peshkov@skbkontur.ru>
2018-10-03 19:06:17 +0500

    add info about DefaultRequestStrategy in IClusterClient.SendAsync

--- Vostok.ClusterClient.Core/IClusterClient.cs
+++ Vostok.ClusterClient.Core/IClusterClient.cs
@@ -20,6 +20,7 @@ namespace Vostok.ClusterClient.Core
         /// <summary>
         /// <para>Sends given request using given <paramref name="parameters"/>, <paramref name="timeout"/> and <paramref name="cancellationToken"/>.</para>
         /// <para>Uses <see cref="IClusterClientConfiguration.DefaultTimeout"/> if timeout in provided <paramref name="parameters"/> is <c>null</c>.</para>
+        /// <para>Uses <see cref="IClusterClientConfiguration.DefaultRequestStrategy"/> if strategy provided <paramref name="parameters"/> is <c>null</c>.</para>
         /// <para>Uses <see cref="IClusterClientConfiguration.DefaultPriority"/> or priority from current ambient context if priority in provided <paramref name="parameters"/> is <c>null</c> (explicit > default > context).</para>
         /// <para>See <see cref="IRequestStrategy.SendAsync"/> for more details about what a request strategy is.</para>
         /// <para>See <see cref="Strategy"/> class for some prebuilt strategies and convenient factory methods.</para>
