81f409539bdea249a8dfa204b5bd99eed2e0eb0b
epeshk <peshkov@skbkontur.ru>
2018-10-03 16:06:46 +0500

    remove implementation specific docs from IClusterClient interface.

--- Vostok.ClusterClient.Core/ClusterClient.cs
+++ Vostok.ClusterClient.Core/ClusterClient.cs
@@ -6,11 +6,33 @@ using Vostok.ClusterClient.Core.Model;
 using Vostok.ClusterClient.Core.Modules;
 using Vostok.ClusterClient.Core.Ordering.Storage;
 using Vostok.ClusterClient.Core.Topology;
+using Vostok.ClusterClient.Core.Transforms;
+using Vostok.ClusterClient.Core.Transport;
 using Vostok.Logging.Abstractions;
 
 namespace Vostok.ClusterClient.Core
 {
-    /// <inheritdoc />
+    /// <summary>
+    /// <para>Represents a client used to send HTTP requests to a cluster of replicas.</para>
+    /// <para>This implementation guarantees following contracts:</para>
+    /// <list type="bullet">
+    /// <item><description>It never throws exceptions. All failures are logged and reflected in returned <see cref="ClusterResult"/> instances.</description></item>
+    /// <item><description>It is thread-safe. It's recommended to reuse <see cref="ClusterClient"/> instances as much as possible.</description></item>
+    /// <item><description>It sends requests with absolute urls directly and does not perform implicit resolving. You can turn them into relative ones with <see cref="IRequestTransform"/>.</description></item>
+    /// </list>
+    /// <para>A <see cref="ClusterClient"/> instance is constructed by passing an <see cref="ILog"/> and a <see cref="ClusterClientSetup"/> delegate to a constructor.</para>
+    /// <para>Provided setup delegate is expected to initialize some fields of an <see cref="IClusterClientConfiguration"/> instance.</para>
+    /// <para>The required minimum is to set <see cref="ITransport"/> and <see cref="IClusterProvider"/> implementations.</para>
+    /// <example>
+    /// <code>
+    /// var client = new ClusterClient(log, config =>
+    /// {
+    ///     config.Transport = new MyTransport();
+    ///     config.ClusterProvider = new MyClusterProvider();
+    /// }
+    /// </code>
+    /// </example>
+    /// </summary>
     [PublicAPI]
     public class ClusterClient : IClusterClient
     {
