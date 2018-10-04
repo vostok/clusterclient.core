9356c263e4d22941372c3c225525e75249e7054b
epeshk <peshkov@skbkontur.ru>
2018-10-04 11:58:01 +0500

    use fixed precision for Request-Timeout header value

--- Vostok.ClusterClient.Core/Transport/RequestTimeoutTransport.cs
+++ Vostok.ClusterClient.Core/Transport/RequestTimeoutTransport.cs
@@ -18,7 +18,7 @@ namespace Vostok.ClusterClient.Core.Transport
 
         public Task<Response> SendAsync(Request request, TimeSpan timeout, CancellationToken cancellationToken)
             => transport.SendAsync(
-                request.WithHeader(HeaderNames.RequestTimeout, timeout.TotalSeconds.ToString(CultureInfo.InvariantCulture)),
+                request.WithHeader(HeaderNames.RequestTimeout, timeout.TotalSeconds.ToString("0.###", NumberFormatInfo.InvariantInfo)),
                 timeout,
                 cancellationToken);
     }
