e969d68811fbcee017a36f09c8b0c6ddf8ddf45e
epeshk <peshkov@skbkontur.ru>
2018-10-10 16:21:30 +0500

    add test about RequestModules ordering

---
+++ Vostok.ClusterClient.Core.Tests/Modules/RequestModules_Tests.cs
@@ -0,0 +1,33 @@
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Clusterclient.Core.Modules;
+using Vostok.Clusterclient.Core.Ordering.Storage;
+using Vostok.Logging.Abstractions;
+
+namespace Vostok.Clusterclient.Core.Tests.Modules
+{
+    public class RequestModules_Tests
+    {
+        [Test]
+        public void Members_of_RequestModules_should_be_ordered_in_pipeline_order()
+        {
+            var configuration = new ClusterClientConfiguration(new SilentLog());
+
+            configuration.ServiceName = string.Empty;
+            
+            configuration.SetupAdaptiveThrottling();
+            configuration.SetupReplicaBudgeting();
+            configuration.SetupHttpMethodValidation();
+
+            var chain = RequestModuleChainBuilder.BuildChain(configuration, new PerInstanceReplicaStorageProvider());
+
+            var values = (RequestModule[]) Enum.GetValues(typeof(RequestModule));
+
+            values.Should().HaveCount(chain.Count);
+            
+            for (var i = 0; i < values.Length; i++)
+                RequestModulesMapping.GetModuleType(values[i]).Should().Be(chain[i].GetType());
+        }
+    }
+}
