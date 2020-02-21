483680a85975f9bbbfb3c2db7ad58bb51576998f
deniaa <deniaa@skbkontur.ru>
2020-02-21 17:15:29 +0500

    ConnectionAttemptsTransport is PublicAPI.

--- Vostok.ClusterClient.Core/Transport/ConnectionAttemptsTransport.cs
+++ Vostok.ClusterClient.Core/Transport/ConnectionAttemptsTransport.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Vostok.Clusterclient.Core.Model;
 using Vostok.Commons.Time;
 
@@ -9,6 +10,7 @@ namespace Vostok.Clusterclient.Core.Transport
     /// <summary>
     /// A transport decorator responsible for retrying connection failures.
     /// </summary>
+    [PublicAPI]
     public class ConnectionAttemptsTransport : ITransport
     {
         private readonly ITransport transport;
