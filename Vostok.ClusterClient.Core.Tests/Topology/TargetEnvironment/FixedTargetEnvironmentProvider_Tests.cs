4737f439221c529df48a9adb670de1230aede9a4
alexey.smirnov <alexey.smirnov@skbkontur.ru>
2020-09-16 13:52:38 +0500

    INFRA-3720: added FixedTargetEnvironmentProvider

---
+++ Vostok.ClusterClient.Core.Tests/Topology/TargetEnvironment/FixedTargetEnvironmentProvider_Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using Vostok.Clusterclient.Core.Topology.TargetEnvironment;
+
+namespace Vostok.Clusterclient.Core.Tests.Topology.TargetEnvironment
+{
+    [TestFixture]
+    internal class FixedTargetEnvironmentProvider_Tests
+    {
+        [Test]
+        public void Should_ensure_that_provided_environment_is_not_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new FixedTargetEnvironmentProvider(null));
+        }
+
+        [TestCase("environment1", ExpectedResult = "environment1")]
+        [TestCase("environment2", ExpectedResult = "environment2")]
+        [TestCase("default", ExpectedResult = "default")]
+        public string Should_return_fixed_environment_value(string environment)
+        {
+            return new FixedTargetEnvironmentProvider(environment).Find();
+        }
+    }
+}
---
+++ Vostok.ClusterClient.Core/Topology/TargetEnvironment/FixedTargetEnvironmentProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Clusterclient.Core.Topology.TargetEnvironment
+{
+    /// <summary>
+    /// Represents a target environment provider which always returns a fixed target environment
+    /// </summary>
+    public class FixedTargetEnvironmentProvider : ITargetEnvironmentProvider
+    {
+        [NotNull]
+        private readonly string environment;
+
+        /// <param name="environment">The environment name this provider should return.</param>
+        public FixedTargetEnvironmentProvider([NotNull] string environment)
+        {
+            this.environment = environment ?? throw new ArgumentNullException(nameof(environment));
+        }
+
+        /// <inheritdoc />
+        public string Find() => environment;
+    }
+}
