2d83000ef21cdd9c5fda71ff0fa38473db320375
shirman <shirman@skbkontur.ru>
2021-05-19 13:06:00 +0500

    RelativeWeightSettings: change defaults

--- Vostok.ClusterClient.Core/Ordering/Weighed/Relative/RelativeWeightSettings.cs
+++ Vostok.ClusterClient.Core/Ordering/Weighed/Relative/RelativeWeightSettings.cs
@@ -8,16 +8,16 @@ namespace Vostok.Clusterclient.Core.Ordering.Weighed.Relative
     {
         public int PenaltyMultiplier = 100;
         public double MinWeight = 0.001;
-        public double InitialWeight = 0.5;
+        public double InitialWeight = 1;
         public double Sensitivity = 4;
         public double RegenerationRatePerMinute = 0.05;
         public double WeightByStatusesRpsThreshold = 1;
-        public TimeSpan WeightUpdatePeriod = TimeSpan.FromSeconds(3);
+        public TimeSpan WeightUpdatePeriod = TimeSpan.FromSeconds(10);
         public TimeSpan WeightsTTL = TimeSpan.FromMinutes(10);
-        public TimeSpan RegenerationLag = TimeSpan.FromMinutes(3);
-        public TimeSpan StatisticTTL = TimeSpan.FromMinutes(30);
-        public TimeSpan StatisticSmoothingConstant = TimeSpan.FromSeconds(5);
-        public TimeSpan WeightsDownSmoothingConstant = TimeSpan.FromSeconds(3);
+        public TimeSpan RegenerationLag = TimeSpan.FromMinutes(1);
+        public TimeSpan StatisticTTL = TimeSpan.FromMinutes(10);
+        public TimeSpan StatisticSmoothingConstant = TimeSpan.FromSeconds(1);
+        public TimeSpan WeightsDownSmoothingConstant = TimeSpan.FromSeconds(1);
         public TimeSpan WeightsRaiseSmoothingConstant = TimeSpan.FromMinutes(1);
     }
 }
