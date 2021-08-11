a4b4e345abddef5701a10389ef3213d8c13243fb
shirman <shirman@skbkontur.ru>
2021-08-11 10:58:28 +0500

    fix

--- Vostok.ClusterClient.Core/Ordering/Weighed/Relative/RelativeWeightCalculator.cs
+++ Vostok.ClusterClient.Core/Ordering/Weighed/Relative/RelativeWeightCalculator.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using Vostok.Clusterclient.Core.Ordering.Weighed.Relative.Interfaces;
+using Vostok.Logging.Abstractions;
 
 namespace Vostok.Clusterclient.Core.Ordering.Weighed.Relative
 {
     internal class RelativeWeightCalculator : IRelativeWeightCalculator
     {
         private readonly RelativeWeightSettings settings;
+        private readonly ILog log;
 
-        public RelativeWeightCalculator(RelativeWeightSettings settings) =>
+        public RelativeWeightCalculator(RelativeWeightSettings settings, ILog log = null)
+        {
             this.settings = settings;
+            this.log = log ?? new SilentLog();
+        }
 
         public Weight Calculate(in AggregatedStatistic clusterAggregatedStatistic, in AggregatedStatistic replicaAggregatedStatistic, in Weight previousWeight)
         {
             var weightByStatuses = clusterAggregatedStatistic.TotalCount / settings.WeightUpdatePeriod.TotalSeconds <= settings.WeightByStatusesRpsThreshold;
+            
             var rawWeight = weightByStatuses
                 ? WeighingHelper.ComputeWeightByStatuses(replicaAggregatedStatistic.TotalCount, replicaAggregatedStatistic.ErrorFraction, clusterAggregatedStatistic.TotalCount, clusterAggregatedStatistic.ErrorFraction, settings.Sensitivity)
                 : WeighingHelper.ComputeWeightByLatency(replicaAggregatedStatistic.Mean, replicaAggregatedStatistic.StdDev, clusterAggregatedStatistic.Mean, clusterAggregatedStatistic.StdDev, settings.Sensitivity);
+            
+            log.Info($"WeightsByStatuses: {weightByStatuses}, {rawWeight}");
             
             var newWeight = EnforceRelativeLimits(rawWeight);
             var smoothingConstant = newWeight > previousWeight.Value
