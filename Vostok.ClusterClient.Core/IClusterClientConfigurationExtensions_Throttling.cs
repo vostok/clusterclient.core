05ac14492f7d62e7c9128ea14443789599cd5b2c
kulbachnyi.v <kulbachnyi.v@skbkontur.ru>
2023-12-06 09:29:43 +0500

    feat: added settings by request priority

--- Vostok.ClusterClient.Core/IClusterClientConfigurationExtensions_Throttling.cs
+++ Vostok.ClusterClient.Core/IClusterClientConfigurationExtensions_Throttling.cs
@@ -30,6 +30,28 @@ namespace Vostok.Clusterclient.Core
 
             configuration.AddRequestModule(new AdaptiveThrottlingModule(options), typeof(AbsoluteUrlSenderModule));
         }
+        
+        /// <summary>
+        /// Sets up an adaptive client throttling mechanism with given parameters.
+        /// </summary>
+        /// <param name="configuration">A configuration to be modified.</param>
+        /// <param name="storageKey">See <see cref="AdaptiveThrottlingOptions.StorageKey"/>.</param>
+        /// <param name="criticalOptions">See <see cref="AdaptiveThrottlingOptions"/>.</param>
+        /// <param name="ordinaryOptions">See <see cref="AdaptiveThrottlingOptions"/>.</param>
+        /// <param name="sheddableOptions">See <see cref="AdaptiveThrottlingOptions"/>.</param>
+        public static void SetupAdaptiveThrottling(
+            this IClusterClientConfiguration configuration,
+            string storageKey,
+            AdaptiveThrottlingOptions criticalOptions,
+            AdaptiveThrottlingOptions ordinaryOptions,
+            AdaptiveThrottlingOptions sheddableOptions)
+        {
+            var defaultOptions = new AdaptiveThrottlingOptions(storageKey);
+            criticalOptions ??= defaultOptions;
+            ordinaryOptions ??= defaultOptions;
+            sheddableOptions ??= defaultOptions;
+            configuration.AddRequestModule(new AdaptiveThrottlingModule(storageKey, criticalOptions, ordinaryOptions, sheddableOptions), typeof(AbsoluteUrlSenderModule));
+        }
 
         /// <summary>
         /// <para>Sets up an adaptive client throttling mechanism with given parameters using <see cref="IClusterClientConfiguration.TargetServiceName"/> and <see cref="IClusterClientConfiguration.TargetEnvironment"/> as a storage key.</para>
@@ -50,6 +72,26 @@ namespace Vostok.Clusterclient.Core
             var storageKey = GenerateStorageKey(configuration);
 
             SetupAdaptiveThrottling(configuration, storageKey, minutesToTrack, minimumRequests, criticalRatio, maximumRejectProbability);
+        }
+        
+        /// <summary>
+        /// <para>Sets up an adaptive client throttling mechanism with given parameters using <see cref="IClusterClientConfiguration.TargetServiceName"/> and <see cref="IClusterClientConfiguration.TargetEnvironment"/> as a storage key.</para>
+        /// <para> <b>N.B.</b> Ensure that <see cref="IClusterClientConfiguration.TargetServiceName"/> and <see cref="IClusterClientConfiguration.TargetEnvironment"/> is set before calling this method.</para>
+        /// </summary>
+        /// <param name="configuration">A configuration to be modified.</param>
+        /// <param name="storageKey">See <see cref="AdaptiveThrottlingOptions.StorageKey"/>.</param>
+        /// <param name="criticalOptions">See <see cref="AdaptiveThrottlingOptions"/>.</param>
+        /// <param name="ordinaryOptions">See <see cref="AdaptiveThrottlingOptions"/>.</param>
+        /// <param name="sheddableOptions">See <see cref="AdaptiveThrottlingOptions"/>.</param>
+        public static void SetupAdaptiveThrottling(
+            this IClusterClientConfiguration configuration,
+            AdaptiveThrottlingOptions criticalOptions,
+            AdaptiveThrottlingOptions ordinaryOptions,
+            AdaptiveThrottlingOptions sheddableOptions)
+        {
+            var storageKey = GenerateStorageKey(configuration);
+
+            SetupAdaptiveThrottling(configuration, storageKey, criticalOptions, ordinaryOptions, sheddableOptions);
         }
     }
 }
