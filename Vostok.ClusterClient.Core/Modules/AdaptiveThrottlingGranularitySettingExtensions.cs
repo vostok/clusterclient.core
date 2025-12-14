02afa7495bfc5cc5cf99c354eaa71de2d0632397
d.khrustalev <d.khrustalev@skbkontur.ru>
2025-12-14 10:39:56 +0500

    refactor: extensions to separate file

---
+++ Vostok.ClusterClient.Core/Modules/AdaptiveThrottlingGranularitySettingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using Vostok.Clusterclient.Core.Model;
+using Vostok.Commons.Collections;
+
+namespace Vostok.Clusterclient.Core.Modules;
+
+[PublicAPI]
+public static class AdaptiveThrottlingGranularitySettingExtensions {
+    public static RequestParameters WithAdaptiveThrottlingGranularity(this RequestParameters parameters, IReadOnlyDictionary<string, string> additionalGranularityKeys)
+    {
+        var immutableGranularity = new ImmutableArrayDictionary<string, string>(additionalGranularityKeys);
+        return parameters.WithProperty(AdaptiveThrottlingModule.RequestParametersStatisticsGranularityPropertyKey, immutableGranularity);
+    }
+
+    public static void SetAdaptiveThrottlingGranularity(this IRequestContext context, IReadOnlyDictionary<string, string> additionalGranularityKeys)
+    {
+        context.Parameters = context.Parameters.WithAdaptiveThrottlingGranularity(additionalGranularityKeys);
+    }
+}
--- Vostok.ClusterClient.Core/Modules/AdaptiveThrottlingModule.cs
+++ Vostok.ClusterClient.Core/Modules/AdaptiveThrottlingModule.cs
@@ -338,18 +338,4 @@ namespace Vostok.Clusterclient.Core.Modules
 
         #endregion
     }
-    
-    [PublicAPI]
-    public static class AdaptiveThrottlingGranularityRequestPropertiesExtensions {
-        public static RequestParameters WithAdaptiveThrottlingGranularity(this RequestParameters parameters, IReadOnlyDictionary<string, string> additionalGranularityKeys)
-        {
-            var immutableGranularity = new ImmutableArrayDictionary<string, string>(additionalGranularityKeys);
-            return parameters.WithProperty(AdaptiveThrottlingModule.RequestParametersStatisticsGranularityPropertyKey, immutableGranularity);
-        }
-
-        public static void SetAdaptiveThrottlingGranularity(this IRequestContext context, IReadOnlyDictionary<string, string> additionalGranularityKeys)
-        {
-            context.Parameters = context.Parameters.WithAdaptiveThrottlingGranularity(additionalGranularityKeys);
-        }
-    }
 }
